076d9e7767dbaa15355703134be5f44a775c68bc
冰雪一舟\qvbrg <qvbrgw.china@gmail.com>
2016-12-14 00:18:10 +0800

    ## add function: get some base path # 2016/12/13 add function to get some base path of program, such as Home and so on

--- bin/win/start.bat
+++ bin/win/start.bat
@@ -1,9 +1,20 @@
 @echo off
-call :getNodePath
-set NODE_PATH=../../
-set LOG_PATH=../../
+rem ##############################################
+rem #  Description: 
+rem #  return code: 3 - get program path failed
+rem ##############################################
 
-rem node /home/ubuntu/node/proc.js >> /home/ubuntu/node/log/proc_log
+setlocal EnableDelayedExpansion
+
+rem === set base path of files ===
+call :getNodePath NODE_PATH
+set LOG_PATH=!NODE_PATH!\log
+
+
+echo !NODE_PATH!
+echo !LOG_PATH!
+
+rem node !NODE_PATH!\web.js >>!LOG_PATH!\web.log 2>>!LOG_PATH!\web.err
 
 
 pause
@@ -14,8 +25,12 @@ rem ##########################   function Line   ##########################
 
 rem ##############################################
 rem #  Description: get program home path
-rem #  Param: 
+rem #  Return: %1 - the path of node program
 rem ##############################################
 :getNodePath
-echo hello
+    if "%1"=="" (
+    	echo getNodePath can not return to a null value
+        exit /b 3
+    )
+    set %~1=%~dp0..\..
 goto :eof
